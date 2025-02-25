bcc7278e9c2c3ddf714a9dfbfc2da515c851ab4c
IdatCicloFinal <Idat6to2025@outlook.com>
2025-02-24 20:46:22 -0500

    mapeo de la tabla cuenta

--- src/main/java/pe/edu/idat/app_transaccion_idat/model/Cuenta.java
+++ src/main/java/pe/edu/idat/app_transaccion_idat/model/Cuenta.java
@@ -1,4 +1,38 @@
 package pe.edu.idat.app_transaccion_idat.model;
 
+import jakarta.persistence.*;
+
+@Table(name = "cuenta")
+@Entity
 public class Cuenta {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private Double saldo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
 }
