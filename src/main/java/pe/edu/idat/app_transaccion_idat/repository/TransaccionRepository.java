93cf08794537096303bd1880f09e651d41913761
IdatCicloFinal <Idat6to2025@outlook.com>
2025-02-24 21:01:40 -0500

    definición del repository

--- src/main/java/pe/edu/idat/app_transaccion_idat/repository/TransaccionRepository.java
+++ src/main/java/pe/edu/idat/app_transaccion_idat/repository/TransaccionRepository.java
@@ -1,4 +1,10 @@
 package pe.edu.idat.app_transaccion_idat.repository;
 
-public interface TransaccionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.idat.app_transaccion_idat.model.Transaccion;
+
+@Repository
+public interface TransaccionRepository
+        extends JpaRepository<Transaccion, Integer> {
 }
